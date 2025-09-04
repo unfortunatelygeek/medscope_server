53c7e5f34757cdc98dd50973b3b61a87931c1caf
unfortunatelygeek <raoaditisudhir@gmail.com>
2025-09-04 15:36:40 +0530

    deploy: render.yaml changed

--- imaging/apps.py
+++ imaging/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class ImagingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'imaging'
+    label = 'imaging'
+    verbose_name = 'Medical Imaging'
